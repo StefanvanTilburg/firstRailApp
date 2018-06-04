6dfc8de3df77aeb870cbe9fa1ab3b293c647c42b
TillytheShark <TillyAliasStefan@gmail.com>
2018-06-04 07:28:23 +0200

    Ch1.3.4 Exc.1-3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world!"
+    render html: "Hola, mundo¡ Or anything else for that mather."
+  end
+
+  def goodbye
+    render html: "Goodbye, world!"
   end
 end
